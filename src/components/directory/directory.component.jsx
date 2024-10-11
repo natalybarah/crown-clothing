01d8752599bd42c1826eab19183a01a6aecc44f9
natalybarah <nathalybs2021@outlook.es>
2024-10-11 06:48:18 -0700

    updating our directory page with directory component and categories

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,17 @@
+
+import CategoryItem from "../category-items/category-item";
+import './directory.styles.scss';
+
+const Directory= ({categories}) => {
+  return(
+    <div  className='directory-container'>
+      {categories.map((category)=>
+      <CategoryItem key={category.id} category={category}/>  )
+      }
+    </div>
+  )
+}
+
+
+
+export default Directory;
